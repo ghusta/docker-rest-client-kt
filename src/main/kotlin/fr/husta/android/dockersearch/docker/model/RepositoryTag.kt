a0264be7ea2c01327e9a6cd9e7a4a74e749604d7
Guillaume HUSTA <guillaume.husta@gmail.com>
2026-03-05 10:23:05 +0100

    feat: Use moshi codegen on data classes
    See: https://github.com/square/moshi#codegen

--- src/main/kotlin/fr/husta/android/dockersearch/docker/model/RepositoryTag.kt
+++ src/main/kotlin/fr/husta/android/dockersearch/docker/model/RepositoryTag.kt
@@ -1,8 +1,10 @@
 package fr.husta.android.dockersearch.docker.model
 
 import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 import java.time.Instant
 
+@JsonClass(generateAdapter = true)
 data class RepositoryTag(
 
     @Json(name = "name")
--- src/main/kotlin/fr/husta/android/dockersearch/docker/model/SearchItemsResponse.kt
+++ src/main/kotlin/fr/husta/android/dockersearch/docker/model/SearchItemsResponse.kt
@@ -1,7 +1,9 @@
 package fr.husta.android.dockersearch.docker.model
 
 import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 
+@JsonClass(generateAdapter = true)
 data class SearchItemsResponse(
 
     @Json(name = "results")
